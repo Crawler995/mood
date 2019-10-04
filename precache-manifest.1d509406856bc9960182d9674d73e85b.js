fa2ca7d1a976ca3ec69e4d1190e30dea2d148a89
crawler995 <zhang_995@foxmail.com>
2019-10-04 19:07:30 +0800

    Updates

---
+++ precache-manifest.1d509406856bc9960182d9674d73e85b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dad5cf93b9dcac88f82cdbfd8492e3f",
+    "url": "/mood-release/index.html"
+  },
+  {
+    "revision": "76f804d9d4e729eee721",
+    "url": "/mood-release/static/css/main.0a5cec9b.chunk.css"
+  },
+  {
+    "revision": "86cf661bb56cf6c186a9",
+    "url": "/mood-release/static/js/2.8c1007f6.chunk.js"
+  },
+  {
+    "revision": "76f804d9d4e729eee721",
+    "url": "/mood-release/static/js/main.b2731348.chunk.js"
+  },
+  {
+    "revision": "2892e806483622e8b8e9",
+    "url": "/mood-release/static/js/runtime~main.1bdbea23.js"
+  }
+]);
